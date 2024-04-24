f549db76257c9d2981113adeb6cbde89391a1461
Eduardo <emariqueo2022@alu.uct.cl>
2024-04-24 00:11:30 -0400

    Login Frontend

--- coffe-s-management/src/Login.jsx
+++ coffe-s-management/src/Login.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
 
 const Login = () => {
     const handleFormSubmit = (e) => {
@@ -9,21 +11,46 @@ const Login = () => {
 
         console.log(email, password);
     };
+
     return (
-        <div className='h-screen flex'>
-            <div className='w-full max-w-md m-auto bg-white rounded-[40px] border border-[#E6D88A] border-4 shadow-default py-10 px-16' style={{boxSizing: 'border-box'}}> {/* Agrega boxSizing: 'border-box' */}
+        <div className='h-screen flex items-center justify-center relative overflow-hidden'>
+            {/* Carrusel de img */}
+            <Carousel
+                autoPlay
+                infiniteLoop
+                showThumbs={false}
+                showStatus={false}
+                showIndicators={false}
+                className="absolute top-0 left-0 w-screen h-screen"
+            >
+                <div>
+                    <img src="p1.jpg" alt="imagen1" style={{ width: '100%', height: '100%' }} />
+                </div>
+                <div>
+                    <img src="p2.jpg" alt="Imagen 2" style={{ width: '100%', height: '100%' }} />
+                </div>
+                <div>
+                    <img src="p3.jpg" alt="Imagen 3" style={{ width: '100%', height: '100%' }} />
+                </div>
+                <div>
+                    <img src="p4.jpg" alt="Imagen 4" style={{ width: '100%', height: '100%' }} />
+                </div>
+            </Carousel>
+
+            {/* Componente de inicio de sesión */}
+            <div className='w-full max-w-md bg-white rounded-[40px] border border-[#E6D88A] border-4 shadow-default py-10 px-16 relative z-10' style={{ fontFamily: 'cursive' }}>
                 <h1 className='text-2xl font-medium text-primary mt-4 mb-12 text-center'>
                     Coffee Admin
                 </h1>
 
                 <form onSubmit={handleFormSubmit}>
                     <div>
-                        <label htmlFor='email'>Correo electronico</label>
+                        <label htmlFor='email'>Correo electrónico</label>
                         <input
                             type='email'
                             className={`w-full p-2 text-primary border rounded-md outline-none text-sm transition duration-150 ease-in-out mb-4`}
                             id='email'
-                            placeholder='Ingresa tu correo electronico'
+                            placeholder='Ingresa tu correo electrónico'
                         />
                     </div>
                     <div>
@@ -36,9 +63,9 @@ const Login = () => {
                         />
                     </div>
 
-                    <div className='flex justify-center items-center mt-6'>
+                    <div className='flex justify-center mt-6'>
                         <button
-                            className={`bg-[#E6D88A]  py-4 px-6 text-sm text-white rounded-[20px] border border-red focus:outline-none focus:border-green-dark`}
+                            className={`bg-[#4A3B25] py-4 px-6 text-sm text-white rounded-[20px] border border-red focus:outline-none focus:border-green-dark`}
                         >
                             Ingresar
                         </button>
