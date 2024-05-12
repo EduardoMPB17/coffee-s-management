5512e0d96b186570bbaf7bbcf21cf3c0793084c8
Cristóbal Medel <crismedel17@gmail.com>
2024-05-11 22:47:31 -0400

    fix: configuración server.js

--- coffe-s-management/server/server.js
+++ coffe-s-management/server/server.js
@@ -1,13 +1,21 @@
 import http from 'http';
 import { connectDB } from '../server/db/db.js';
 
-const server = http.createServer(async (req, res) => {
-  const client = await connectDB();
+const startServer = async () => {
+  try{
+    const server = http.createServer((req, res) => {
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Este es el Backend');
+    });
 
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Este es el Backend');
-});
+    server.listen(3000, () => {
+      console.log('El Servidor está corriendo en: http://localhost:3000/');
+    });
 
-server.listen(3000, () => {
-  console.log('El Servidor esta corriendo en: http://localhost:3000/');
-});
+    const client = await connectDB();
+  } catch (error) {
+    console.error('Error al iniciar el servidor: ', error);
+  }
+};
+
+startServer();
