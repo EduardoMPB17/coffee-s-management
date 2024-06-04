a10716f4ef91bcecb478ec55ad6faa985170e8f4
Eduardo <lalitomariqueo@gmail.com>
2024-06-03 21:34:17 -0400

    avance frontend

--- client/src/components/bar.jsx
+++ client/src/components/bar.jsx
@@ -2,6 +2,8 @@ import React from "react";
 import { MdOutlineEmail } from "react-icons/md";
 import { HiOutlineInformationCircle } from "react-icons/hi2";
 import { IoMdHelpCircleOutline } from "react-icons/io";
+import { Link } from "react-router-dom";
+
 //completar usuario con BD
 const WelcomeBar = ({ username }) => {
   return (
@@ -19,17 +21,20 @@ const WelcomeBar = ({ username }) => {
         >
           <ul className="list-style-none me-auto flex flex-col ps-0 lg:mt-1 lg:flex-row">
             <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0">
+              <Link to={"/Contacto"}>
               <a className="p-0 text-black/60 transition duration-200 hover:text-black/80 focus:text-black/80 active:text-black/80 lg:px-2"
-                href="#"><MdOutlineEmail className="inline-block mr-1 text-2xl" />
+                ><MdOutlineEmail className="inline-block mr-1 text-2xl" />
                 Contactanos
-                
               </a>
+              </Link>
             </li>
             <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0">
+              <Link to={"/Info"}>
               <a className="p-0 text-black/60 transition duration-200 hover:text-black/80 focus:text-black/80 active:text-black/80 lg:px-2"
                 href="#"><HiOutlineInformationCircle className="inline-block mr-1 text-2xl"></HiOutlineInformationCircle>
                 Acerca de Nosotros
               </a>
+              </Link>
             </li>
             <li className="mb-4 ps-2 lg:mb-0 lg:pe-1 lg:ps-0 ">
               <a className="p-0 text-black/60 transition duration-200 hover:text-black/80 focus:text-black/80 active:text-black/80 lg:px-2"
--- client/src/pages/Estadisticas.jsx
+++ client/src/pages/Estadisticas.jsx
@@ -12,7 +12,8 @@ function Estadistics() {
             </header>
             <nav className='row-start-1 row-span-3'> <Sidebar/> </nav>
             <main className='col-start-2 flex justify-center items-center relative'>
-                <img src='/img/p1.jpg' alt='background' className='absolute inset-0 w-full h-full object-cover object-center' />
+                
+                <img src='/img/p1.jpg' alt='background' className='absolute inset-0 w-full h-full object-cover object-center ' />
                 <div className="grid grid-cols-2 gap-4 relative">
                     <div class="bg-[#4A3B25] text-[#E6D88A] border-4 border-[#E6D88A] py-24 px-28 rounded-md font-semibold text-xl hover:bg-[#5e4a2c] transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 transition duration-300 hover:rotate-180">
                         <div class="flex h-full justify-center items-center">
