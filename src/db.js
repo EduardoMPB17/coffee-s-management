0ca9dee7b96230a0b973d0c923b85a34e86fddcc
Eduardo <lalitomariqueo@gmail.com>
2024-06-05 21:12:05 -0400

    mongodb cluster

--- src/db.js
+++ src/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
     try{
-        await mongoose.connect('mongodb://Amaro:123@ac-vgutnvs-shard-00-00.haejzuj.mongodb.net:27017,ac-vgutnvs-shard-00-01.haejzuj.mongodb.net:27017,ac-vgutnvs-shard-00-02.haejzuj.mongodb.net:27017/?ssl=true&replicaSet=atlas-wao00r-shard-0&authSource=admin&retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://Eduardo:123@cluster0.xb1udix.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log(">> DB conectada")
     }catch (error){
         console.log(error);
