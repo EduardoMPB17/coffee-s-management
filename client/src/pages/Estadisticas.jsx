ad010f41c79b515aa4d456de92f150abed1b4ae8
Eduardo <emariqueo2022@alu.uct.cl>
2024-07-01 21:44:30 -0400

    Revert "Merge remote-tracking branch 'origin/Cristobal' into Eduardo"
    This reverts commit f33282e71fd1a999131611d7075e96ffdb187c09, reversing
    changes made to 7559f6252ca5201bfe79b62cd061282c8c61c082.

--- client/src/pages/Estadisticas.jsx
+++ client/src/pages/Estadisticas.jsx
@@ -20,33 +20,27 @@ function Estadistics() {
                         <button >Ventas Mensuales</button>
                         </div>
                     </div>
-                    <Link to='/Ventas'>
-                    <button>
-                        <div class="bg-[#4A3B25] text-[#E6D88A] border-4 border-[#E6D88A] py-24 px-28 rounded-md font-semibold text-xl hover:bg-[#5e4a2c] transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 transition duration-300 hover:rotate-180">
-                            <div class="flex h-full justify-center items-center">
-                            Productos más vendidos
-                            </div>
+                    <div class="bg-[#4A3B25] text-[#E6D88A] border-4 border-[#E6D88A] py-24 px-28 rounded-md font-semibold text-xl hover:bg-[#5e4a2c] transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 transition duration-300 hover:rotate-180">
+                        <div class="flex h-full justify-center items-center">
+                        <Link to='/Ventas'>
+                        <button>Productos más vendidos</button>
+                        </Link>
                         </div>
-                    </button>
-                    </Link>
-                    <Link to='/Crecimiento'>
-                    <button>
-                        <div class="bg-[#4A3B25] text-[#E6D88A] border-4 border-[#E6D88A] py-24 px-28 rounded-md font-semibold text-xl hover:bg-[#5e4a2c] transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 transition duration-300 hover:rotate-180">
-                            <div class="flex h-full justify-center items-center">
-                            Crecimiento
-                            </div>
+                    </div>
+                    <div class="bg-[#4A3B25] text-[#E6D88A] border-4 border-[#E6D88A] py-24 px-28 rounded-md font-semibold text-xl hover:bg-[#5e4a2c] transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 transition duration-300 hover:rotate-180">
+                        <div class="flex h-full justify-center items-center">
+                        <Link to='/Crecimiento'>
+                        <button>Crecimiento</button>
+                        </Link>
                         </div>
-                    </button>
-                    </Link>
-                    <Link to='/Grafico'>
-                    <button>
-                        <div class="bg-[#4A3B25] text-[#E6D88A] border-4 border-[#E6D88A] py-24 px-28 rounded-md font-semibold text-xl hover:bg-[#5e4a2c] transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 transition duration-300 hover:rotate-180">
-                            <div class="flex h-full justify-center items-center">
-                            Gráficos
-                            </div>
+                    </div>
+                    <div class="bg-[#4A3B25] text-[#E6D88A] border-4 border-[#E6D88A] py-24 px-28 rounded-md font-semibold text-xl hover:bg-[#5e4a2c] transform  rounded-xl h-40 w-40 sm:h-64 sm:w-64 transition duration-300 hover:rotate-180">
+                        <div class="flex h-full justify-center items-center">
+                        <Link to = '/Grafico'>
+                        <button>Gráficos</button>
+                        </Link>
                         </div>
-                    </button>
-                    </Link>
+                    </div>
                 </div>
             </main>
             <div className='col-start-2'> <Footer/> </div>
